17f3aa0fba8eeb7047cd12e650eefeccb10b6b5a
MagadouP <152959026+MagadouP@users.noreply.github.com>
2024-02-23 16:19:51 +0100

    Update 01-color-switcher.js
    update 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,4 +2,9 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     }
 
-  
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  const body = document.body;
+
+let intervalId = 0;
+
